9233076cc93fa1d8b6e4d1d4a8ed96eb3845469a
McConnell <ik.ugwuanyi@gmail.com>
2022-11-04 05:31:19 +0100

    stage 2 task

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+
+class OperationTypeEnum(str, Enum):
+    addtition = "addition"
+    subtraction = "subtraction"
+    multiplication = "multiplication"
+
+class OperationSchema(BaseModel):
+    operation_type: OperationTypeEnum
+    x: int
+    y: int
 
 app = FastAPI()
 
 @app.get("/")
 async def home():
     bio = "I am a backend developer, with experience in python web frameworks like django, flask and fastap"
-    return { "slackUsername": "McConnell", "backend": True, "age": 22, "bio": bio}
+    return { "slackUsername": "iConnell", "backend": True, "age": 22, "bio": bio}
+
+@app.post("/eval")
+async def eval(input: OperationSchema):
+    result = 0
+    if input.operation_type == 'addition':
+        result = input.x + input.y
+    elif input.operation_type == 'subtraction':
+        result = input.x - input.y
+    elif input.operation_type == 'multiplication':
+        result = input.x * input.y
+    else: raise HTTPException(400, "Invalid operation type")
+
+    operation_type = input.operation_type
+
+    return { "slackUsernam": "iConnell", "operation_type" : operation_type, "result": result }
